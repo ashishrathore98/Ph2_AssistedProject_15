f0658d79a5505483e25531fccbefa379321212ed
Ashish Rathore <ashrathore98@gmail.com>
2023-09-05 02:04:40 +0530

    first commit

---
+++ AssistedProjects_15/Controllers/DefaultController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using AssistedProjects_15.Models;
+using System.Security.Claims;
+using System.Web.Mvc;
+
+
+namespace AssistedProjects_15.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
---
+++ AssistedProjects_15/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AssistedProjects_15.MvcApplication" Language="C#" %>
